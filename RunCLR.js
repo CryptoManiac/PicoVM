eaba3711caded6741e49624c6af9f0f15cdfa0ba
CryptoManiac <balthazar@yandex.ru>
2016-09-28 12:34:24 +0300

    Replace tests.

--- RunCLR.js
+++ RunCLR.js
@@ -5,7 +5,7 @@
 const AppDomain = require('./CLR/AppDomain');
 
 var appDomain = new AppDomain(1);
-appDomain.loadAssembly("Program.exe", function (a) {
+appDomain.loadAssembly("Fib.exe", function (a) {
     if (a == undefined) {
         console.log('Empty program');
         return;
