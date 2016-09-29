dc5dab75c6e0dce7822a6c3a336cc01f9946b32a
CryptoManiac <balthazar@yandex.ru>
2016-09-29 15:03:29 +0300

    Simple string arguments testing program.

--- RunCLR.js
+++ RunCLR.js
@@ -5,7 +5,7 @@
 const AppDomain = require('./CLR/AppDomain');
 
 var appDomain = new AppDomain();
-appDomain.loadAssembly("StringArg.exe", function (a) {
+appDomain.loadAssembly("Fib.exe", function (a) {
     if (a == undefined) {
         console.log('Empty program');
         return;
